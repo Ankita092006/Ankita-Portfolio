cdf013bef9bfa3a6009fdb9c10c70dc1bbc16e18
Ankita Sasmal <sasmalankita09@gmail.com>
2026-05-02 16:30:20 +0530

    Ankita

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,9 @@
 import React from "react";
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
+import project3 from "../assets/project3.png";
+import project4 from "../assets/project4.png";
+import project5 from "../assets/project5.png";
 function Projects() {
   return (
     <div style={{ padding: "40px" }}>
@@ -28,28 +31,28 @@ function Projects() {
       
       {/* Project 3 */}
       <div style={{ marginTop: "20px" }}>
-        <h3>Tic Tac Toe</h3>
-        <p>
-         This is a simple Tic Tac Toe game built using HTML, CSS, and JavaScript, allowing two players to take turns and determine a winner based on game rules.
-        </p>
-        <p><b>Technology:</b> HTML5,CSS3,JS</p>
-      </div>
-  
-      {/* Project 4 */}
-      <div style={{ marginTop: "20px" }}>
-        <h3>Snake Game</h3>
-        <p>
-          This is a simple Snake game built using HTML, CSS, and JavaScript, where the player controls a snake to collect food and grow while avoiding collisions.
-        </p>
-        <p><b>Technology:</b> HTML5,CSS3,JS</p>
-      </div>
-     {/* Project 5 */}
-      <div style={{ marginTop: "20px" }}>
         <h3>Amazon Clone Website</h3>
         <p>
          This is a simple Amazon clone website built using HTML and CSS, displaying products with a basic user interface.
         </p>
         <p><b>Technology:</b> HTML5,CSS3</p>
+      </div>
+  
+      {/* Project 4 */}
+      <div style={{ marginTop: "20px" }}>
+        <h3>Tic Tac Toe</h3>
+        <p>
+          This is a simple Tic Tac Toe game built using HTML, CSS, and JavaScript, allowing two players to take turns and determine a winner based on game rules.
+        </p>
+        <p><b>Technology:</b> HTML5,CSS3,JS</p>
+      </div>
+     {/* Project 5 */}
+      <div style={{ marginTop: "20px" }}>
+        <h3>Snake Game</h3>
+        <p>
+         This is a simple Snake game built using HTML, CSS, and JavaScript, where the player controls a snake to collect food and grow while avoiding collisions.
+        </p>
+        <p><b>Technology:</b> HTML5,CSS3,JS</p>
       </div>
     
 
