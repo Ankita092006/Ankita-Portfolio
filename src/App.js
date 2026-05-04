2ec8cd9619aeccaa9734d91e5cc73277f1d78aa3
Ankita Sasmal <sasmalankita09@gmail.com>
2026-05-04 12:00:27 +0530

    Ankita

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 
 import Home from "./pages/Home";
 import About from "./pages/About";  // ✅ fixed (removed extra text)
